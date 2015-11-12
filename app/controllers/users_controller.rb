dbf22d25d0c3857489639209fa121ec006b4e9b2
Van-Luan <luanhoangmaik6@gmail.com>
2015-11-12 13:33:21 +0700

    finishing

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   before_action :logged_in_user, only: [:index, :edit, :update]
   before_action :correct_user,   only: [:edit, :update]
-  before_action :admin_user,     only:  [:destroy, :edit, :update]
+  
   def index
     @users = User.paginate(page: params[:page])
   end
@@ -62,10 +62,9 @@ class UsersController < ApplicationController
       if current_user.admin?
         return true
       else
-        
-      @user = User.find(params[:id])
-      redirect_to(root_url) unless current_user?(@user)
-    end
+        @user = User.find(params[:id])
+        redirect_to(root_url) unless current_user?(@user)
+      end
     end
     def admin_user
       redirect_to(root_url) unless current_user.admin?
